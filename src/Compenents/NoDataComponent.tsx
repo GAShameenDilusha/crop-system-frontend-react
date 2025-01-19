661407fc4065658348a4829d9e9c6ad454fef45c
shameen <shameendilusha5678@gmail.com>
2025-01-19 13:25:36 +0530

    update NoDataComponent.tsx

---
+++ src/Compenents/NoDataComponent.tsx
@@ -0,0 +1,14 @@
+interface NoDataComponentProps {
+    message: string;
+}
+
+const NoDataComponent = ({ message }: NoDataComponentProps) => {
+    return (
+        <div className="flex flex-col items-center justify-center py-10">
+            <img src="src/assets/images/NodataImg.png" alt="No Data" className="md:w-80 md:h-80 mb-4 w-40 h-40" />
+            <p className="text-gray-500 text-lg">{message}</p>
+        </div>
+    );
+};
+
+export default NoDataComponent;
