f19f78a6bc93c21b61c67492bcbaa77c9a7b2946
shameen <shameendilusha5678@gmail.com>
2025-01-19 12:29:01 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {AuthLayout} from "./Compenents/AuthLayout.tsx";
+import LoginPage from "./Pages/LoginPage.tsx";
+import SignUpPage from "./Pages/SignUpPage.tsx";
+import {Provider} from "react-redux";
+import store from "./Store/Store.ts";
+//import  SidBarComponent from "./Components/SideBarComponent.tsx"
+//import TopHeaderComponent from "./Components/TopHeaderComponent.tsx";
+import {MainLayout} from "./Compenents/MainLayout.tsx";
+import DashboardPage from "./Pages/DashboardPage.tsx";
+import FieldPage from "./Pages/FieldsPage.tsx";
+import CropsPage from "./Pages/CropPage.tsx";
+import StaffPage from "./Pages/StaffPage.tsx";
+import LogsPage from "./Pages/LogsPage.tsx";
+import VehiclePage from "./Pages/VehiclePage.tsx";
+import EquipmentPage from "./Pages/EquipmentPage.tsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const routes = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <AuthLayout />,
+                children: [
+                    { path: "", element: <LoginPage /> }, // Default to LoginPage
+                    { path: "signup", element: <SignUpPage /> },
+                ],
+            },
+            {
+                path: "/", // Important: Same base path as AuthLayout, but different layout
+                element: <MainLayout />,
+                children: [
+                    { path: "dashboard", element: <DashboardPage /> },
+                    { path: "field", element: <FieldPage /> },
+                    { path: "crop", element: <CropsPage /> },
+                    { path: "staff", element: <StaffPage /> },
+                    { path: "logs", element: <LogsPage /> },
+                    { path: "vehicles", element: <VehiclePage /> },
+                    { path: "equipment", element: <EquipmentPage /> },
+                ],
+            }
+        ]
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+            <Provider store={store}>
+                <RouterProvider router={routes} />
+            </Provider>
+
+        </>
+    )
 }
 
 export default App
