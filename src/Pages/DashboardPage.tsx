e3f191fcbf1dbe7541de3be62757e779fceb2593
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:24:21 +0530

    update DashboardPage.tsx

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -10,7 +10,7 @@ export default function DashboardPage(){
 
         <>
             <section>
-                <div className="flex flex-wrap items-center justify-center md:mt-10 md:ml-40 ml-0 mt-14 " >
+                <div className="flex flex-wrap items-center justify-center md:mt-40 md:ml-40 ml-0 mt-14 " >
                     <DashboardCardComponent title="Total Crops" value='20'/>
                     <DashboardCardComponent title="Active Staff" value='18'/>
                     <DashboardCardComponent title="Completed Tasks" value='10'/>
@@ -18,17 +18,17 @@ export default function DashboardPage(){
                 </div>
 
 
-                <div className="relative" >
-                    {/* Weather Card */}
+                {/*<div className="relative" >
+                     Weather Card
                     <div className="relative md:top-10 md:ml-10">
                         <WeatherMap />
                     </div>
 
-                    {/* Task Calendar */}
+                     Task Calendar
                     <div className=" md:absolute md:top-1 md:right-20 min-h-100 justify-center items-center flex  ">
                         <TaskCalendar />
                     </div>
-                </div>
+                </div>*/}
 
 
 
