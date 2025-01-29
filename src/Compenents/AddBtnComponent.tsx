b54dd410bba3124774bb11a18f54634a92d3f874
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:16:23 +0530

    update AddBtnComponent.tsx

--- src/Compenents/AddBtnComponent.tsx
+++ src/Compenents/AddBtnComponent.tsx
@@ -1,8 +1,8 @@
-export default function AddBtnComponent(props: { onClick: () => void, text: string }) {
+export default function AddBtnComponent(props) {
     return (
         <button
             onClick={props.onClick}
-            className="w-60 h-12 font-bold bg-[rgba(242,215,182,0.63)] text-[#274D40] text-lg rounded-lg cursor-pointer transition-all ease-in-out duration-300 shadow-md hover:bg-[#8ad38d] hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#274D40] flex items-center justify-center text-xl"
+            className="w-60 h-11 font-bold bg-gradient-to-b from-white to-green-700 backdrop-blur-sm placeholder-gray-700 text-[#274D40]  rounded-lg cursor-pointer transition-all ease-in-out duration-300 shadow-md hover:bg-[#8ad38d] hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#274D40] flex items-center justify-center text-xl"
         >
             <i className="fas fa-plus-circle mr-2"></i>
             {props.text}
