3fde945534813140556e7328a5b5dc2fb712cf9c
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:20:40 +0530

    update AuthLayout.tsx

--- src/Compenents/AuthLayout.tsx
+++ src/Compenents/AuthLayout.tsx
@@ -2,7 +2,16 @@ import {Outlet} from "react-router-dom";
 
 export function AuthLayout() {
     return (
-        <section className="min-h-screen flex justify-center items-center bg-LogoSectionGreen">
+        /*<section className="min-h-screen flex justify-center items-center bg-LogoSectionGreen">
+            <Outlet/>
+        </section>*/
+        <section
+            className="min-h-screen flex justify-center items-center bg-[url('src/assets/images/background.jpg')] bg-cover bg-center">
+            {/*<img
+                src="src/assets/images/logo.png"
+                alt="Green Shadow Logo"
+                className="w-6/10 mb-2"
+            />*/}
             <Outlet/>
         </section>
     )
