5c3a14fd1c61eed8b08a63a9c3e75d09f4be3f60
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:28:13 +0530

    update LoginPage.tsx

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -27,30 +27,30 @@ export default  function LoginPage() {
         console.log(" go to signing up");
         navigate("signup");
     };
-    return (
-        <>
-            <section className=" flex justify-center items-center bg-gray-50 rounded-lg" >
-                <div
-                    className="max-w-4xl w-full bg-white shadow-lg rounded-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
-                    <LogoSection/>
-                    <div className="p-6 flex flex-col justify-center">
-                        <h3 className="text-3xl font-bold text-center mb-6 text-gray-700">Welcome Back!</h3>
-                        <form>
-                            <SignInInputFields id="login-email" type="text" placeholder="User Name" icon="fas fa-user" options={[]} onChange={(e) => setEmail(e.target.value)} />
-                            <SignInInputFields id="login-password" type="password" placeholder="Password" icon="fas fa-lock" options={[]} onChange={(e) => setPassword(e.target.value)} />
-                            <div className="text-right text-sm mt-2">
-                                <a href="#" className="text-green-500 hover:underline">Forgot password?</a>
-                            </div>
-                            <SignInButtonGroup
-                                buttons={[
-                                    {label: 'Sign In', className: 'bg-emerald-600 text-white shadow-lg', onClick: handleLogin},
-                                    {label: 'Sign Up', className: 'bg-yellow-600 text-white shadow-lg' ,onClick: handleSignUpNavigation},
-                                ]}
-                            />
-                        </form>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+      <>
+          <section className=" flex justify-center items-center bg-gray-50 rounded-lg" >
+              <div
+                  className="max-w-4xl w-full bg-gradient-to-br from-green-600 to-gray-100 shadow-lg rounded-lg overflow-hidden grid grid-cols-1 md:grid-cols-2 ">
+                  <LogoSection/>
+                  <div className="p-6 flex flex-col justify-center">
+                      <h3 className="text-3xl font-bold text-center mb-6 text-gray-700">Welcome Back!</h3>
+                      <form>
+                          <SignInInputFields id="login-email" type="text" placeholder="User Name" icon="fas fa-user" options='' onChange={(e) => setEmail(e.target.value)} />
+                          <SignInInputFields id="login-password" type="password" placeholder="Password" icon="fas fa-lock" options='' onChange={(e) => setPassword(e.target.value)} />
+                          <div className="text-right text-sm mt-2">
+                              <a href="#" className="text-green-500 hover:underline">Forgot password?</a>
+                          </div>
+                          <SignInButtonGroup
+                              buttons={[
+                                  {label: 'Sign In', className: 'bg-emerald-600 text-white shadow-lg', onClick: handleLogin},
+                                  {label: 'Sign Up', className: 'bg-yellow-600 text-white shadow-lg' ,onClick: handleSignUpNavigation},
+                              ]}
+                          />
+                      </form>
+                  </div>
+              </div>
+          </section>
+      </>
+  )
 }
