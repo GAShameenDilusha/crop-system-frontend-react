09e28a0119ac03c10f52d2a7118c3e2b04e86cff
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:40:00 +0530

    update Store.ts

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,11 +1,23 @@
 import {configureStore} from "@reduxjs/toolkit";
 
 import authReducer from "../Features/AuthSlice.ts";
+import fieldReducer from "../Features/FieldSlice.ts";
+import cropReducer from "../Features/CropSlice.ts";
+import staffReducer from "../Features/StaffSlice.ts";
+import vehicleReducer from "../Features/VehicleSlice.ts";
+import equipmentReducer from "../Features/EquipmentSlice.ts";
+import logReducer from "../Features/LogSlice.ts";
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
+        field:fieldReducer,
+        crop:cropReducer,
+        staff:staffReducer,
+        vehicle:vehicleReducer,
+        equipment:equipmentReducer,
+        log:logReducer
+
     }
 });
 export default store;
-
