d2e0b06e8d7e43cf571b6a68202a77e6c6985955
shameen <shameendilusha5678@gmail.com>
2025-01-29 09:28:32 +0530

    update LogoSection.tsx

--- src/Compenents/LogoSection.tsx
+++ src/Compenents/LogoSection.tsx
@@ -1,17 +1,17 @@
 export  default function LogoSection () {
     return (
         <>
-            <div className="bg-gradient-to-t from-LoginBackgroundColor to-white shadow  flex flex-col justify-center items-center p-2 pb-0 ">
+            <div className="bg-gradient-to-br from-green-600 to-green-200  to-white shadow  flex flex-col justify-center items-center p-2 pb-0 ">
                 <img
-                    src="src/assets/images/greenshadowlogo-02.png"
+                    src="src/assets/images/Green-logo%202.png"
                     alt="Green Shadow Logo"
                     className="w-6/10 mb-2"
                 />
-                <img
-                    src="src/assets/images/depositphotos_7151451-stock-photo-plant-in-hands-removebg-preview.png"
+                {/*<img
+                    src="src/assets/images/background.jpg"
                     alt="Hand with Plant"
                     className="w-100 mr-5 mb-0 md:block"
-                />
+                />*/}
             </div>
         </>
     )
